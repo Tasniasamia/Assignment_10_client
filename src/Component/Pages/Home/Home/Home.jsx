5401cf67d5c657f44a0118a0a8a326bd2e2c0722
tasniaarham <tasniaarhamarham@gmail.com>
2023-05-03 00:20:26 +0600

    banner_correction

--- src/Component/Pages/Home/Home/Home.jsx
+++ src/Component/Pages/Home/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
-
+import Carousel from 'react-bootstrap/Carousel';
 import Row from 'react-bootstrap/Row';
 import './Home.css'
 import { useLoaderData } from 'react-router-dom';
@@ -28,9 +28,29 @@ const Home = () => {
         <div className='my-auto'>
         <h1 style={{fontSize:"65px",color:"brown"}}>Baked</h1>
         <h2 className='text-danger'>no matter how you cook, Yumma has many the recipes</h2>
-        <p style={{color:"white"}}>Restaurant food recipes are recipes of dishes that are commonly served in restaurants. These dishes can vary greatly depending on the type of restaurant and the cuisine it specializes in. Restaurant food recipes can be complex, using a variety of ingredients and cooking techniques to create unique and delicious flavors. They can also be simple, using just a few ingredients to create classic dishes that are loved by many. Many restaurants keep their recipes a closely guarded secret, but some chefs and restaurants are willing to share their recipes with the public.Restaurant food is known for its wide variety of flavors, textures, and presentation. From Michelin-starred fine dining establishments to local diners, restaurants offer a range of cuisines and dishes to satisfy every palate. One of the reasons people enjoy eating at restaurants is the chance to try new and exciting dishes that they may not be able to recreate at home. Many restaurants also source high-quality ingredients from local farms and artisanal producers to create fresh, flavorful, and healthy dishes.Restaurant food is known for its wide variety of flavors, textures, and presentation. From Michelin-starred fine dining establishments to local diners, restaurants offer a range of cuisines and dishes to satisfy every palate. One of the reasons people enjoy eating at restaurants is the chance to try new and exciting dishes that they may not be able to recreate at home. Many restaurants also source high-quality ingredients from local farms and artisanal producers to create fresh, flavorful, and healthy dishes.In addition to the food itself, the atmosphere and experience of dining out at a restaurant are also part of the appeal. Whether you are enjoying a romantic dinner with your partner, celebrating a special occasion with friends and family, or grabbing a quick bite to eat on your lunch break, the ambiance and service of a restaurant can greatly enhance the overall experience.
+        <p style={{color:"white"}}>Restaurant food recipes are recipes of dishes that are commonly served in restaurants. These dishes can vary greatly depending on the type of restaurant and the cuisine it specializes in. Restaurant food recipes can be complex, using a variety of ingredients and cooking techniques to create unique and delicious flavors.</p>        <Button variant="danger">Learn More</Button>
 
-However, eating at restaurants too frequently can be costly and may not always be the healthiest option. It is important to balance dining out with home-cooked meals and to be mindful of portion sizes and ingredients when eating at restaurants. Overall, restaurant food offers a unique and enjoyable dining experience that can be appreciated in moderation.</p>        <Button variant="danger">Learn More</Button>
+
+        <Container className='my-5'>
+
+<div className='d-md-flex justify-content-evenly align-items-center '>
+    <div className='p-3'>
+<img className='img-fluid rounded' src="https://as1.ftcdn.net/v2/jpg/00/86/55/48/1000_F_86554865_TH1mGC85z9BYsQwNwvF2vQgsoNfgLqUf.jpg" alt="foods" />
+    </div>
+    <div className='p-3'>
+    <img  className='img-fluid rounded'src="https://img.freepik.com/free-photo/spicy-hot-pot_74190-4240.jpg?w=1060&t=st=1682981526~exp=1682982126~hmac=30cf99fc527bc418112a2cd72d689234147b8af5288de42546d5863906608704" alt="foods" />
+    </div>
+    <div className='p-3'>
+    <img className='img-fluid rounded'src="https://as2.ftcdn.net/v2/jpg/01/86/01/59/1000_F_186015989_8YtUXBlmZDLPWmcIyr6NriY0ErP3O08N.jpg" alt="foods" />
+    </div>
+    <div className='p-3'>
+    <img className='img-fluid rounded'src="https://as1.ftcdn.net/v2/jpg/03/52/93/40/1000_F_352934068_tOxY9HRQULtSPob74c1OhySe3CStiFiR.jpg" alt="foods" />
+    </div>
+</div></Container>
+
+
+
+
 </div>
       </div>
     </div>
@@ -40,11 +60,24 @@ However, eating at restaurants too frequently can be costly and may not always b
       <h1 style={{fontSize:"65px",color:"brown"}}>Boild</h1>       
       <h2 className='text-danger'>no matter how you cook, Yumma has many the recipes</h2>
 
-       <p style={{color:"white"}}className='my-5'>Restaurant food recipes are recipes of dishes that are commonly served in restaurants. These dishes can vary greatly depending on the type of restaurant and the cuisine it specializes in. Restaurant food recipes can be complex, using a variety of ingredients and cooking techniques to create unique and delicious flavors. They can also be simple, using just a few ingredients to create classic dishes that are loved by many. Many restaurants keep their recipes a closely guarded secret, but some chefs and restaurants are willing to share their recipes with the public.Restaurant food is known for its wide variety of flavors, textures, and presentation. From Michelin-starred fine dining establishments to local diners, restaurants offer a range of cuisines and dishes to satisfy every palate. One of the reasons people enjoy eating at restaurants is the chance to try new and exciting dishes that they may not be able to recreate at home. Many restaurants also source high-quality ingredients from local farms and artisanal producers to create fresh, flavorful, and healthy dishes.Restaurant food is known for its wide variety of flavors, textures, and presentation. From Michelin-starred fine dining establishments to local diners, restaurants offer a range of cuisines and dishes to satisfy every palate. One of the reasons people enjoy eating at restaurants is the chance to try new and exciting dishes that they may not be able to recreate at home. Many restaurants also source high-quality ingredients from local farms and artisanal producers to create fresh, flavorful, and healthy dishes.In addition to the food itself, the atmosphere and experience of dining out at a restaurant are also part of the appeal. Whether you are enjoying a romantic dinner with your partner, celebrating a special occasion with friends and family, or grabbing a quick bite to eat on your lunch break, the ambiance and service of a restaurant can greatly enhance the overall experience.
-
-However, eating at restaurants too frequently can be costly and may not always be the healthiest option. It is important to balance dining out with home-cooked meals and to be mindful of portion sizes and ingredients when eating at restaurants. Overall, restaurant food offers a unique and enjoyable dining experience that can be appreciated in moderation.</p>
+       <p style={{color:"white"}}className='my-5'>Restaurant food recipes are recipes of dishes that are commonly served in restaurants. These dishes can vary greatly depending on the type of restaurant and the cuisine it specializes in. Restaurant food recipes can be complex, using a variety of ingredients and cooking techniques to create unique and delicious flavors. </p>
        <Button variant="danger">Learn More</Button>
+       <Container className='my-5'>
 
+<div className='d-md-flex justify-content-evenly align-items-center '>
+    <div className='p-3'>
+<img className='img-fluid rounded' src="https://as1.ftcdn.net/v2/jpg/00/86/55/48/1000_F_86554865_TH1mGC85z9BYsQwNwvF2vQgsoNfgLqUf.jpg" alt="foods" />
+    </div>
+    <div className='p-3'>
+    <img  className='img-fluid rounded'src="https://img.freepik.com/free-photo/spicy-hot-pot_74190-4240.jpg?w=1060&t=st=1682981526~exp=1682982126~hmac=30cf99fc527bc418112a2cd72d689234147b8af5288de42546d5863906608704" alt="foods" />
+    </div>
+    <div className='p-3'>
+    <img className='img-fluid rounded'src="https://as2.ftcdn.net/v2/jpg/01/86/01/59/1000_F_186015989_8YtUXBlmZDLPWmcIyr6NriY0ErP3O08N.jpg" alt="foods" />
+    </div>
+    <div className='p-3'>
+    <img className='img-fluid rounded'src="https://as1.ftcdn.net/v2/jpg/03/52/93/40/1000_F_352934068_tOxY9HRQULtSPob74c1OhySe3CStiFiR.jpg" alt="foods" />
+    </div>
+</div></Container>
       </div>
     </div>
     <div className="carousel-item">
@@ -53,22 +86,46 @@ However, eating at restaurants too frequently can be costly and may not always b
       <h1 style={{fontSize:"65px",color:"brown"}}>Streamed</h1>       
       <h2 className='text-danger'>no matter how you cook, Yumma has many the recipes</h2>
 
-        <p style={{color:"white"}}>Restaurant food recipes are recipes of dishes that are commonly served in restaurants. These dishes can vary greatly depending on the type of restaurant and the cuisine it specializes in. Restaurant food recipes can be complex, using a variety of ingredients and cooking techniques to create unique and delicious flavors. They can also be simple, using just a few ingredients to create classic dishes that are loved by many. Many restaurants keep their recipes a closely guarded secret, but some chefs and restaurants are willing to share their recipes with the public.Restaurant food is known for its wide variety of flavors, textures, and presentation. From Michelin-starred fine dining establishments to local diners, restaurants offer a range of cuisines and dishes to satisfy every palate. One of the reasons people enjoy eating at restaurants is the chance to try new and exciting dishes that they may not be able to recreate at home. Many restaurants also source high-quality ingredients from local farms and artisanal producers to create fresh, flavorful, and healthy dishes.Restaurant food is known for its wide variety of flavors, textures, and presentation. From Michelin-starred fine dining establishments to local diners, restaurants offer a range of cuisines and dishes to satisfy every palate. One of the reasons people enjoy eating at restaurants is the chance to try new and exciting dishes that they may not be able to recreate at home. Many restaurants also source high-quality ingredients from local farms and artisanal producers to create fresh, flavorful, and healthy dishes.In addition to the food itself, the atmosphere and experience of dining out at a restaurant are also part of the appeal. Whether you are enjoying a romantic dinner with your partner, celebrating a special occasion with friends and family, or grabbing a quick bite to eat on your lunch break, the ambiance and service of a restaurant can greatly enhance the overall experience.
-
-However, eating at restaurants too frequently can be costly and may not always be the healthiest option. It is important to balance dining out with home-cooked meals and to be mindful of portion sizes and ingredients when eating at restaurants. Overall, restaurant food offers a unique and enjoyable dining experience that can be appreciated in moderation.</p>
+        <p style={{color:"white"}}>Restaurant food recipes are recipes of dishes that are commonly served in restaurants. These dishes can vary greatly depending on the type of restaurant and the cuisine it specializes in. Restaurant food recipes can be complex, using a variety of ingredients and cooking techniques to create unique and delicious flavors.  </p>
         <Button variant="danger">Learn More</Button>
+        <Container className='my-5'>
+
+<div className='d-md-flex justify-content-evenly align-items-center '>
+    <div className='p-3'>
+<img className='img-fluid rounded' src="https://as1.ftcdn.net/v2/jpg/00/86/55/48/1000_F_86554865_TH1mGC85z9BYsQwNwvF2vQgsoNfgLqUf.jpg" alt="foods" />
+    </div>
+    <div className='p-3'>
+    <img  className='img-fluid rounded'src="https://img.freepik.com/free-photo/spicy-hot-pot_74190-4240.jpg?w=1060&t=st=1682981526~exp=1682982126~hmac=30cf99fc527bc418112a2cd72d689234147b8af5288de42546d5863906608704" alt="foods" />
+    </div>
+    <div className='p-3'>
+    <img className='img-fluid rounded'src="https://as2.ftcdn.net/v2/jpg/01/86/01/59/1000_F_186015989_8YtUXBlmZDLPWmcIyr6NriY0ErP3O08N.jpg" alt="foods" />
+    </div>
+    <div className='p-3'>
+    <img className='img-fluid rounded'src="https://as1.ftcdn.net/v2/jpg/03/52/93/40/1000_F_352934068_tOxY9HRQULtSPob74c1OhySe3CStiFiR.jpg" alt="foods" />
+    </div>
+</div></Container>
       </div>
     </div>
   </div>
   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="carousel-control-prev-icon " aria-hidden="true"></span>
     <span className="visually-hidden">Previous</span>
   </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+  <button className="carousel-control-next " type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+    <span className="carousel-control-next-icon " aria-hidden="true"></span>
     <span className="visually-hidden">Next</span>
   </button>
 </div></div>
+
+
+
+
+
+
+
+
+
+
 
 
 {/* card-start */}
